6f22f13e64de5b9f2cc78526cfa213d72627a672
Charlie Dudzik <dudzikc@outlook.com>
2025-04-11 22:10:48 -0400

    Update

--- lua/cd-4/lazy/misc.lua
+++ lua/cd-4/lazy/misc.lua
@@ -1,6 +1,7 @@
 return {
-  { "tpope/vim-eunuch" },
-  { "tpope/vim-endwise" },
-  { "tpope/vim-repeat" },
+  { "tpope/vim-eunuch", lazy = false },
+  { "tpope/vim-endwise", lazy = false },
+  { "tpope/vim-repeat", lazy = false },
+  { "tpope/vim-sleuth", lazy = false },
   { "airblade/vim-gitgutter" },
 }
--- lua/cd-4/set.lua
+++ lua/cd-4/set.lua
@@ -9,6 +9,7 @@ vim.opt.softtabstop = 4
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
+vim.opt.autoindent = true
 
 vim.opt.wrap = true
 
@@ -17,3 +18,7 @@ vim.opt.scrolloff = 8
 vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
+
+vim.cmd([[
+  filetype plugin indent on
+]])
