d815e05bb289a99245d0243f4462f48d70f07e15
Charlie Dudzik <dudzikc@outlook.com>
2025-04-12 22:07:07 -0400

    Update

--- lua/cd-4/lazy/local.lua
+++ lua/cd-4/lazy/local.lua
@@ -1,9 +1,19 @@
 return {
+  --[[
   {
     "git-needy.nvim",
     dir = "~/repos/git-needy.nvim",
     config = function()
       require("git-needy").setup()
+    end,
+  },
+  --]]
+  {
+    "cd-4/git-needy.nvim",
+    config = function()
+      require("git-needy").setup({
+        repos = {},
+      })
     end,
   },
 }
