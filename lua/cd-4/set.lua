65762f40177d2fb9e654ed2274a5a4a3a09499b1
Charlie Dudzik <dudzikc@outlook.com>
2025-04-12 23:49:54 -0400

    Update

--- lua/cd-4/set.lua
+++ lua/cd-4/set.lua
@@ -8,6 +8,16 @@ vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
 vim.opt.expandtab = true
 
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "markdown", "javascript", "typescript", "javascriptreact", "typescriptreact" },
+  callback = function()
+    vim.bo.tabstop = 2
+    vim.bo.shiftwidth = 2
+    vim.bo.softtabstop = 2
+    vim.bo.expandtab = true
+  end,
+})
+
 -- vim.opt.smartindent = true
 -- vim.opt.autoindent = true
 
