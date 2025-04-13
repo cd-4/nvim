702a0d90aeced0e0ddb1795fa1d49f216b397f6f
Charlie Dudzik <dudzikc@outlook.com>
2025-04-13 12:01:29 -0400

    Update to have completion

---
+++ lua/cd-4/lazy/nvim-cmp.lua
@@ -0,0 +1,52 @@
+return {
+  "hrsh7th/nvim-cmp",
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp", -- LSP completion source
+    "hrsh7th/cmp-buffer", -- Buffer words
+    "hrsh7th/cmp-path", -- File system paths
+    "hrsh7th/cmp-cmdline", -- Command line
+    "L3MON4D3/LuaSnip", -- Snippets
+    "saadparwaiz1/cmp_luasnip", -- Snippet completion
+  },
+  config = function()
+    local cmp = require("cmp")
+    local luasnip = require("luasnip")
+
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
+      mapping = cmp.mapping.preset.insert({
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      }),
+      sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
+      }, {
+        { name = "buffer" },
+        { name = "path" },
+      }),
+    })
+  end,
+}
