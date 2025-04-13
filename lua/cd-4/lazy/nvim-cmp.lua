b13f933f64233f9d6093427566d6a3eda465242d
Charlie Dudzik <dudzikc@outlook.com>
2025-04-13 12:11:26 -0400

    Update to use autocmpletion

--- lua/cd-4/lazy/nvim-cmp.lua
+++ lua/cd-4/lazy/nvim-cmp.lua
@@ -21,7 +21,8 @@ return {
       mapping = cmp.mapping.preset.insert({
         ["<C-Space>"] = cmp.mapping.complete(),
         ["<CR>"] = cmp.mapping.confirm({ select = true }),
-        ["<Tab>"] = cmp.mapping(function(fallback)
+        -- Next Suggestion
+        ["<C-j>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_next_item()
           elseif luasnip.expand_or_jumpable() then
@@ -30,7 +31,8 @@ return {
             fallback()
           end
         end, { "i", "s" }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
+        -- Prev Suggestion
+        ["<C-k>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_prev_item()
           elseif luasnip.jumpable(-1) then
